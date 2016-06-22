614ecabefb8ceb87ef5b2cd3f884b5d327ea241e
Chris Edwards <screwtop.github@ripples.dyndns.org>
2016-06-22 18:16:59 +1200

    Finalising firmware template, including adding missing pin mode settings for the servo outputs

--- Projects/2016/MIDIBots/arduino/firmware_template/src/sketch.ino
+++ Projects/2016/MIDIBots/arduino/firmware_template/src/sketch.ino
@@ -45,6 +45,7 @@ void flash(int on_time, int off_time) {
 
 // Routine for displaying a number by blinking the LED (since hardware serial will be taken already by the MIDI comms):
 void flash_number(int n) {
+	delay(500);
 	// Hundreds:
 	for (int i = 0; i < (n % 1000 / 100); i++) {
 		flash(800, 800);
@@ -59,11 +60,13 @@ void flash_number(int n) {
 	for (int i = 0; i < (n % 10); i++) {
 		flash(200, 200);
 	}
+	delay(500);
 }
 
 
 // Read MIDI channel DIP switches and store the result:
 // NOTE: Freetronics USBDroid has a pull-down resistor on pin D4, which makes that pin unusable! It acts as if D4 is always grounded, i.e. the switch is on.
+// Multiply the 4x bit by 4 on a normal Arduino, or 0 to force it off.
 void read_MIDI_channel() {
 	MIDI_channel =
 		!digitalRead(MIDI_8x_PIN) * 8 +
@@ -74,48 +77,61 @@ void read_MIDI_channel() {
 
 void self_test() {
 	// Robot-specific self-test routine goes here
+//	flash(50, 50); flash(50, 50); flash(50, 50); flash(50, 50);
+	read_MIDI_channel();
+	flash_number(MIDI_channel);
 }
-
-// TODO: interrupt handling for pushing the self-test button
 
 void setup()
 {
 	// Set up pin modes for the MIDIBot Shield (input, internal pull-up resistor enabled):
-	pinMode(MIDI_1x_PIN, INPUT); digitalWrite(MIDI_1x_PIN, HIGH);
-	pinMode(MIDI_2x_PIN, INPUT); digitalWrite(MIDI_2x_PIN, HIGH);
-	pinMode(MIDI_4x_PIN, INPUT); digitalWrite(MIDI_4x_PIN, HIGH);
-	pinMode(MIDI_8x_PIN, INPUT); digitalWrite(MIDI_8x_PIN, HIGH);
+	pinMode(MIDI_1x_PIN, INPUT_PULLUP);
+	pinMode(MIDI_2x_PIN, INPUT_PULLUP);
+	pinMode(MIDI_4x_PIN, INPUT_PULLUP);
+	pinMode(MIDI_8x_PIN, INPUT_PULLUP);
 	
 	// Ditto for self-test button:
-	pinMode(SELF_TEST_PIN, INPUT); digitalWrite(SELF_TEST_PIN, HIGH);
+	pinMode(SELF_TEST_PIN, INPUT_PULLUP);
 	
 	// TODO: output pins (servos, MOSFETs, LED)
 	pinMode(LED_PIN, OUTPUT); digitalWrite(LED_PIN, LOW);
+	pinMode(MOSFET_PWM_PIN, OUTPUT); analogWrite(MOSFET_PWM_PIN, 0);
 	pinMode(MOSFET_2_PIN, OUTPUT); digitalWrite(MOSFET_2_PIN, LOW);
 	pinMode(MOSFET_3_PIN, OUTPUT); digitalWrite(MOSFET_3_PIN, LOW);
 	pinMode(MOSFET_4_PIN, OUTPUT); digitalWrite(MOSFET_4_PIN, LOW);	
-	
+	pinMode(SERVO_1_PIN, OUTPUT); digitalWrite(SERVO_1_PIN, LOW);
+	pinMode(SERVO_2_PIN, OUTPUT); digitalWrite(SERVO_2_PIN, LOW);
+	pinMode(SERVO_3_PIN, OUTPUT); digitalWrite(SERVO_3_PIN, LOW);
+
 	// Initialise MIDI channel number according to DIP switch settings:
 	read_MIDI_channel();
+	flash_number(MIDI_channel);
 	
 	// Set up MIDI communication:
 	Serial.begin(31250);
 	clearData();
-
+	
+	// Attach interrupt for self-test button and function:
+//	attachInterrupt(digitalPinToInterrupt(SELF_TEST_PIN), self_test, FALLING);
+	// Only available on certain pins?  Just poll in main loop instead.
+	
 	// Flash to indicate startup/ready:
 	flash(50, 200); flash(50, 400); flash(50, 200); flash(400, 0);
 }
 
 void loop()
 {
-	process_MIDI();
+	if (!digitalRead(SELF_TEST_PIN)) {
+		self_test();
+	}
+//	process_MIDI();
 //	test_blink();
 //	test_button();
 //	test_flash_number();
 //	test_MIDI_channel();
 //	test_MOSFETs();
 //	test_MOSFETs_cycle();
-//	test_PWM();
+	test_PWM();
 }
 
 void process_MIDI() {
