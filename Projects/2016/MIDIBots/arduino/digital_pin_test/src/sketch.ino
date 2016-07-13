3c44a6ab65bbfa41850097753c9c22b1ef16199c
Chris Edwards <screwtop.github@ripples.dyndns.org>
2016-07-13 14:24:44 +1200

    Expanded digital pin test to flash a pin several times, and to test a range of pins in one implementation

--- Projects/2016/MIDIBots/arduino/digital_pin_test/src/sketch.ino
+++ Projects/2016/MIDIBots/arduino/digital_pin_test/src/sketch.ino
@@ -1,16 +1,34 @@
-const int TEST_PIN = 10;
+// We've had so many issues with flaky digital pins that it's probably worth having a test program that toggles each one a few times for easy testing on the oscilloscope...
+
+
+const int FIRST_PIN = 8;
+const int LAST_PIN = 13;
 
 
 void setup()
 {
-	pinMode(TEST_PIN, OUTPUT);
-	digitalWrite(TEST_PIN, LOW);
+	for (int pin = FIRST_PIN; pin <= LAST_PIN; pin++) {
+		pinMode(pin, OUTPUT);
+		digitalWrite(pin, LOW);
+	}
 }
 
 void loop()
 {
-	digitalWrite(TEST_PIN, HIGH);
-	delay(500);
-	digitalWrite(TEST_PIN, LOW);
-	delay(500);
+	for (int pin = FIRST_PIN; pin <= LAST_PIN; pin++) {
+		multiflash(pin, 4);
+	}
+}
+
+void multiflash(int pin, int times) {
+	for (int i = 0; i < times; i++) {
+		flash(pin);
+	}
+}
+
+void flash(int pin) {
+	digitalWrite(pin, HIGH);
+	delay(100);
+	digitalWrite(pin, LOW);
+	delay(150);
 }
