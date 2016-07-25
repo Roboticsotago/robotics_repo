263a6f00e8242777ce155dd96ddbfd7ac7baf76e
Chris Edwards <chris.edwards@otago.ac.nz>
2016-07-25 15:48:23 +1200

    Added initial Arduino library for our MIDIBot shields

---
+++ Projects/2016/MIDIBots/arduino/libraries/MIDIBot/MIDIBot.cpp
@@ -0,0 +1,165 @@
+/*
+ * Arduino library for our MIDIBot shield circuit board, Robocup 2016
+ * Team _underscore_, Information Science Mechatronics, University of Otago
+*/
+
+#include "Arduino.h"
+#include "MIDIBot.h"
+
+
+// Constructor:
+// At least some of the setup routine will be standard, but some firmware-specific setup code will also be likely.
+void MIDIBot::MIDIBot()
+{
+	// Set up pin modes for the MIDIBot Shield (input, internal pull-up resistor enabled):
+	pinMode(MIDI_1x_PIN, INPUT_PULLUP);
+	pinMode(MIDI_2x_PIN, INPUT_PULLUP);
+	pinMode(MIDI_4x_PIN, INPUT_PULLUP);
+	pinMode(MIDI_8x_PIN, INPUT_PULLUP);
+	
+	// Ditto for self-test button:
+	pinMode(SELF_TEST_PIN, INPUT_PULLUP);
+	
+	// TODO: output pins (servos, MOSFETs, LED)
+	pinMode(LED_PIN, OUTPUT); digitalWrite(LED_PIN, LOW);
+	pinMode(MOSFET_PWM_PIN, OUTPUT); analogWrite(MOSFET_PWM_PIN, 0);
+	pinMode(MOSFET_2_PIN, OUTPUT); digitalWrite(MOSFET_2_PIN, LOW);
+	pinMode(MOSFET_3_PIN, OUTPUT); digitalWrite(MOSFET_3_PIN, LOW);
+	pinMode(MOSFET_4_PIN, OUTPUT); digitalWrite(MOSFET_4_PIN, LOW);	
+	pinMode(SERVO_1_PIN, OUTPUT); digitalWrite(SERVO_1_PIN, LOW);
+	pinMode(SERVO_2_PIN, OUTPUT); digitalWrite(SERVO_2_PIN, LOW);
+	pinMode(SERVO_3_PIN, OUTPUT); digitalWrite(SERVO_3_PIN, LOW);
+	
+	// Initialise MIDI channel number according to DIP switch settings:
+	read_MIDI_channel();
+	flash_number(_MIDI_channel);
+	
+	// Set up MIDI communication:
+	Serial.begin(31250);
+	clearData();
+	
+	// Attach interrupt for self-test button and function:
+//	attachInterrupt(digitalPinToInterrupt(SELF_TEST_PIN), self_test, FALLING);
+	// Only available on certain pins?  Just poll in main loop instead.
+	
+	// Flash to indicate startup/ready:
+//	flash(50, 200); flash(50, 400); flash(50, 200); flash(400, 0);
+	// Don't bother if flashing MIDI channel - it's confusing!
+}
+
+
+
+void MIDIBot::clearData(){
+	_dataByte[0] = 0;
+	_dataByte[1] = 0;
+	_i = 0;
+}
+
+
+void MIDIBot::flash(int on_time, int off_time) {
+	digitalWrite(LED_PIN, HIGH);
+	delay(on_time);
+	digitalWrite(LED_PIN, LOW);
+	delay(off_time);	
+}
+
+
+// Routine for displaying a number by blinking the LED (since hardware serial will be taken already by the MIDI comms):
+void MIDIBot::flash_number(int n) {
+	delay(500);
+	// Hundreds:
+	for (int i = 0; i < (n % 1000 / 100); i++) {
+		flash(800, 800);
+	}
+	delay(500);
+	// Tens:
+	for (int i = 0; i < (n % 100 / 10); i++) {
+		flash(400, 400);
+	}
+	delay(500);
+	// Fastest flashing for the ones:
+	for (int i = 0; i < (n % 10); i++) {
+		flash(200, 200);
+	}
+	delay(1000);
+}
+
+
+// Read MIDI channel DIP switches and store the result:
+// NOTE: Freetronics USBDroid has a pull-down resistor on pin D4, which makes that pin unusable! It acts as if D4 is always grounded, i.e. the switch is on.
+// Multiply the 4x bit by 4 on a normal Arduino, or 0 to force it off.
+void MIDIBot::read_MIDI_channel() {
+	_MIDI_channel =
+		!digitalRead(MIDI_8x_PIN) * 8 +
+		!digitalRead(MIDI_4x_PIN) * 4 +
+		!digitalRead(MIDI_2x_PIN) * 2 +
+		!digitalRead(MIDI_1x_PIN);
+}
+
+
+// self_test() will be specific to each robot, but a reasonable basis would be to re-read the MIDI channel from the DIP switches and flash the number.
+//void self_test() {
+	// Robot-specific self-test routine goes here
+//	read_MIDI_channel();
+//	flash_number(_MIDI_channel + 1);
+//}
+
+
+// Can we factor out some parts of process_MIDI into common functions, and make process_MIDI generic?  Perhaps define constants such as NOTE_ON?
+// We could have it expect functions note_on(pitch, velocity), note_off(pitch, velocity), but I think these will need prototype/forward declarations.
+// TODO: copy _dataByte[0..1] to meaningfully-named variables (pitch/note, velocity) for better readability?
+void process_MIDI() {
+	if (Serial.available() > 0) {
+		int data = Serial.read();
+		if (data > 127) {
+			// It's a status byte. Store it for future reference.
+			_statusByte = data;
+			clearData();  
+		} else {
+			// It's a data byte.
+			_dataByte[i] = data;
+			if (_statusByte == (0x90 | _MIDI_channel) && i == 1) {
+				// Note-on message received
+			//	flash(20,0); // Warning: blocks!
+				if (_dataByte[1] == 0) {
+					// Note-on with velocity=0: Stop note playing (nothing to do for percussion!)
+					note_off(_dataByte[0], _dataByte[1]);
+				} else {
+					// Start note playing
+					note_on(_dataByte[0], _dataByte[1]);
+				}
+			} else if (_statusByte == (0x80 | _MIDI_channel) && i == 1) {
+				// Note-off message received
+				note_off(_dataByte[0], _dataByte[1]);
+			}
+			i++;
+			// TODO: error detection if i goes beyond the array size.
+		}
+	}
+}
+
+
+// Some standard self-test routines:
+void MIDIBot::test_blink() {
+	digitalWrite(LED_PIN, HIGH);
+	delay(500);
+	digitalWrite(LED_PIN, LOW);
+	delay(500);
+}
+
+
+void MIDIBot::test_button() {
+	digitalWrite(LED_PIN, !digitalRead(SELF_TEST_PIN));
+}
+
+
+void MIDIBot::test_MIDI_channel() {
+	if (!digitalRead(SELF_TEST_PIN)) {
+		read_MIDI_channel();
+		Serial.print(_MIDI_channel);
+		Serial.print(" (");
+		Serial.print(_MIDI_channel + 1);
+		Serial.println(")");
+		flash_number(_MIDI_channel + 1);
+	}
+}
---
+++ Projects/2016/MIDIBots/arduino/libraries/MIDIBot/MIDIBot.h
@@ -0,0 +1,65 @@
+/*
+ * Arduino library for our MIDIBot shield circuit board, Robocup 2016
+ * Team _underscore_, Information Science Mechatronics, University of Otago
+*/
+
+// TODO:
+// [ ] Have the use of Serial be generic?  Or maybe it's OK to hardcode to use the hardware serial always, since we're dealing with hardware shields.
+// [ ] Convert to camelCase throughout for function names?  It's recommended in the Arduino API style guide.  Also member variables?
+// [ ] Rename _i to something more meaningful, e.g. _dataByteIndex
+// [ ] #define constants?  That seems to be the norm with .h files, and they'd only be defined if the header were included, and might save on processor registers?
+
+#ifndef MIDIBot_included
+#define MIDIBot_included
+
+#include "Arduino.h"
+
+// Define pin mappings for the MIDIBot shield:
+// For a library, should these be global symbols or defined within the class?
+// Also, how do we make the class a singleton?
+
+// Should these be consts or #defines?
+const int
+	MIDI_1x_PIN = 2,
+	MIDI_2x_PIN = 3,
+	MIDI_4x_PIN = 4,	/* USBDroid has pulldown resistor on D4! */
+	MIDI_8x_PIN = 5,
+	MOSFET_PWM_PIN = 6,
+	SERVO_1_PIN = 9,	/* USBDroid has pulldown resistor on D9! */
+	SERVO_2_PIN = 10,
+	SERVO_3_PIN = 11,	/* USBDroid has pulldown resistor on D11! */
+	MOSFET_2_PIN = 12,
+	MOSFET_3_PIN = A1,	/* not A3 as on the silkscreen! */
+	MOSFET_4_PIN = A2,	/* not A4 as on the silkscreen! */
+	LED_PIN = 13,		/* USBDroid has pulldown resistor on D13! */
+	LATENCY_ADJUST_PIN = A0,
+	SELF_TEST_PIN = A5
+;
+
+class MIDIBot {
+	public:
+		MIDIBot();	// Class-specific setup(), essentially. Maybe pass Serial object?  Or hard-code to that internally?  Given hardware is hardwired, maybe OK to do that.
+		void clearData();
+		void flash(int on_time, int off_time);
+		void flash_number(int n);
+		void read_MIDI_channel();
+		void self_test();	// self_test() will be specific to each robot, but a reasonable basis would be to re-read the MIDI channel from the DIP switches and flash the number.
+		void process_MIDI();	// Main MIDI-processing "loop"
+	//	void note_off(int note, int velocity);	// Define note_on/off within the class and require user to subclass, or just assume externally defined?
+	//	void note_on(int note, int velocity);
+		// Generic self-test functions:
+		void test_blink();
+		void test_button();
+		void test_MIDI_channel();
+	private:
+		int _MIDI_channel = 0;	// 1..16, 10=drums
+		
+		// MIDI messages have up to 2 data bytes, which we store in an array:
+		// TODO: perhaps name the index more sensibly/explicitly:
+		int _dataByte[2], _statusByte = 0, _i = 0;
+		
+		// For monophonic instruments, it's useful to keep track of the note currently being played, so that any note-off messages for other notes can be ignored.
+		int _current_note_number = 0;
+};
+
+#endif
