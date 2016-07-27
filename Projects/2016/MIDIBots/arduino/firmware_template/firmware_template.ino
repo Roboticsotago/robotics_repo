a77b30b8a8542d8310f405014dc4500bc7fa0293
Chris Edwards <chris.edwards@otago.ac.nz>
2016-07-28 01:00:30 +1200

    Revamped example firmware template after the example I wrote for documenting the library (see MIDIBot.h)

--- Projects/2016/MIDIBots/arduino/firmware_template/firmware_template.ino
+++ Projects/2016/MIDIBots/arduino/firmware_template/firmware_template.ino
@@ -1,39 +1,29 @@
-// Template/skeleton code for the 2016 MIDIBots
+// ____Bot code for the 2016 MIDIBots
 // Team _underscore_, Information Science Mechatronics, University of Otago
 
 #include <MIDIBot.h>
-
-// Additional functions required to be defined by the MIDIBot library:
+MIDIBot thisMIDIBot;
 
 void note_on(int note, int velocity) {
-	digitalWrite(LED_PIN, HIGH);
-	analogWrite(MOSFET_PWM_PIN, 64);
+	// TODO...
 }
 
 void note_off(int note, int velocity) {
-	digitalWrite(LED_PIN, LOW);
-	analogWrite(MOSFET_PWM_PIN, 0);
+	// TODO...
 }
 
-
-// Instantiate the object from the library:
-
-MIDIBot testMIDIBot;
-
-// NOTE: you have to define self_test() after the library has been instantiated, otherwise the library object's functions won't be available.
 void self_test() {
-	// Robot-specific self-test routine goes here. Example:
-	if (!digitalRead(SELF_TEST_PIN)) {
-		testMIDIBot.test_MIDI_channel();
-	}
+//	thisMIDIBot.test_MIDI_channel();
+	// TODO: Bot-specific self-test routine...
 }
 
 void setup() {
-	testMIDIBot.test_MIDI_channel();
+	thisMIDIBot.test_MIDI_channel();	// Indicate MIDI channel at startup
 }
 
 void loop() {
-	testMIDIBot.test_button();
-//  self_test();
-//  testMIDIBot.test_blink();
+	if (!digitalRead(SELF_TEST_PIN)) {
+		self_test();
+	}
+	thisMIDIBot.process_MIDI();
 }
