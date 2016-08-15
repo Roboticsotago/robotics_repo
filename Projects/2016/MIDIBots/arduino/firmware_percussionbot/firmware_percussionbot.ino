7c9df66b087caf4ee6278dfa086f6d91c0cd2d08
Chris Edwards <screwtop.github@ripples.dyndns.org>
2016-08-15 18:17:22 +1200

    Changed PercussionBot firmware to support separate notes for right and left shaker movement

--- Projects/2016/MIDIBots/arduino/firmware_percussionbot/firmware_percussionbot.ino
+++ Projects/2016/MIDIBots/arduino/firmware_percussionbot/firmware_percussionbot.ino
@@ -1,6 +1,8 @@
 // PercussionBot code for the 2016 MIDIBots
 // Team _underscore_, Information Science Mechatronics, University of Otago
 
+// TODO: implement seperate right/left command notes for the shaker.
+
 // TODO: add a DC motor operating a football/cog rattle? And a similar mechanism for repeatedly striking the triangle?
 
 #include <MIDIBot.h>
@@ -26,10 +28,17 @@ const int TRIANGLE_MAX = 14;
 const int TRIANGLE_DELAY = 100;
 // Checked!
 
-const int SHAKER_NOTE = 39; //62
-const int SHAKER_MIN = 100;
-const int SHAKER_MAX = 110;
-const int SHAKER_DELAY = 100;
+const int SHAKER_MID = 90;
+
+const int SHAKER_L_NOTE = 39;
+const int SHAKER_R_NOTE = 40;
+
+//const int SHAKER_R_MIN = 100;
+const int SHAKER_L_MAX = 70;
+const int SHAKER_R_MAX = 110;
+
+const int SHAKER_L_DELAY = 100;
+const int SHAKER_R_DELAY = 100;
 // Checked! 
 
 const int DRUM_NOTE = 36; //64
@@ -41,7 +50,8 @@ const int DRUM_DELAY = 100;
 
 // Timers for asynchronous release of drum hits:
 Timer *triangle_timer = new Timer(TRIANGLE_DELAY, &triangle_release, 1);
-Timer *shaker_timer = new Timer(SHAKER_DELAY, &shaker_release, 1);
+//Timer *shaker_l_timer = new Timer(SHAKER_L_DELAY, &shaker_l_release, 1);
+//Timer *shaker_r_timer = new Timer(SHAKER_R_DELAY, &shaker_r_release, 1);
 Timer *drum_timer = new Timer(DRUM_DELAY, &drum_release, 1);
 
 
@@ -50,9 +60,13 @@ void triangle_hit() {
 	triangle_servo.write(TRIANGLE_MAX);
 	triangle_timer->Start();
 }
-void shaker_hit() {
-	shaker_servo.write(SHAKER_MAX);
-	shaker_timer->Start();
+void shaker_l_hit() {
+	shaker_servo.write(SHAKER_L_MAX);
+//	shaker_timer->Start();
+}
+void shaker_r_hit() {
+	shaker_servo.write(SHAKER_R_MAX);
+//	shaker_timer->Start();
 }
 void drum_hit() {
 	drum_servo.write(DRUM_MAX);
@@ -60,13 +74,14 @@ void drum_hit() {
 }
 
 void triangle_release() {triangle_servo.write(TRIANGLE_MIN);}
-void   shaker_release() {  shaker_servo.write(SHAKER_MIN);}
+//void   shaker_release() {  shaker_servo.write(SHAKER_MIN);}
 void     drum_release() {   drum_servo.write(DRUM_MIN);}
 
 void note_on(int note, int velocity) {
 	switch (note) {
 		case TRIANGLE_NOTE: triangle_hit(); break;
-		case SHAKER_NOTE:   shaker_hit();   break;
+		case SHAKER_L_NOTE:   shaker_l_hit();   break;
+		case SHAKER_R_NOTE:   shaker_r_hit();   break;
 		case DRUM_NOTE:     drum_hit();     break;
 	}					
 }
@@ -84,7 +99,7 @@ void test_servo() {
 
 void self_test() {
 	triangle_hit();
-	shaker_hit();
+	shaker_r_hit();
 	drum_hit();
 }
 
@@ -97,7 +112,7 @@ void setup()
 	
 	// Set initial position to _MIN values
 	triangle_servo.write(TRIANGLE_MIN);
-	shaker_servo.write(SHAKER_MIN);
+	shaker_servo.write(SHAKER_MID);
 	drum_servo.write(DRUM_MIN);
 
 	percussionBot.begin();
@@ -106,7 +121,7 @@ void setup()
 void loop()
 {
 	triangle_timer->Update();
-	shaker_timer->Update();
+//	shaker_timer->Update();
 	drum_timer->Update();
 	
 	percussionBot.process_MIDI();
