8e2c34e2dacff21752bc005c424f2ff6d555f97f
Chris Edwards <screwtop.github@ripples.dyndns.org>
2016-08-29 22:02:54 +1200

    Added initial (testing only) note_on() implementation. Have LED light during self-test. Added to-do items.

--- Projects/2016/MIDIBots/arduino/firmware_ukebot/firmware_ukebot.ino
+++ Projects/2016/MIDIBots/arduino/firmware_ukebot/firmware_ukebot.ino
@@ -1,6 +1,12 @@
 // UkeBot code for the 2016 MIDIBots
 // Team _underscore_, Information Science Mechatronics, University of Otago
 
+/*
+TODO:
+[ ] Calibrate slide positions to correspond to musical intervals
+[ ] Add note-on messages for slide positioning
+[ ] Add note-on messages for plucking indiviual strings (think of it as having 5 positions which can be reached: top, between first two strings, between middle strings, between last two strings, and bottom).  Could be tricky to get the timing right, as the time to pluck will depend on the current strumming position.  Perhaps just pick a single value that generally works OK.
+*/
 
 // Define pin mappings for the MIDIBot shield:
 #include <Servo.h> 
@@ -17,11 +23,14 @@ const int DOWN_NOTE = 62; // D
 
 const int STRUM_MIN = 90;
 const int STRUM_MAX = 110;
+// TODO: constants for each inter-string position
+// ...
 
 // For slide max/min, we probably want min to be at the nut end, and max at the soundhole end.
 const int SLIDE_MIN = 60;	// 55 is too far - it can get stuck! 60
 const int SLIDE_MAX = 88;	// 88
 const int SLIDE_HOME = 108; // Somewhere far enough beyond MAX that it will avoid the backlash.  108
+// TODO: constants (or an array?) of servo positions that correspond to musical notes.
 
 int slide_position = SLIDE_MIN;
 
@@ -63,13 +72,20 @@ void move_slide(int pos) {
 	delay(SLIDE_DELAY);
 }
 
-void note_on(int note, int velocity) {}
+void note_on(int note, int velocity) {
+	switch (note) {
+		case UP_NOTE:   up_strum();   break;
+		case DOWN_NOTE: down_strum(); break;
+	}
+}
 
 void note_off(int note, int velocity) {}
+
 
 void setup()
 {
 	UkeBot.begin();
+	
 	// Servo setup:
 	strum.attach(SERVO_3_PIN);
 	slide.attach(SERVO_2_PIN);
@@ -97,6 +113,7 @@ void self_test_2()
 }
 
 void self_test() {
+	digitalWrite(LED_PIN, HIGH);
 	slide_position = SLIDE_MAX;
 	for (i=0; i<=5; i++){
 		move_slide(slide_position);
@@ -106,6 +123,7 @@ void self_test() {
 		delay(500);
 		slide_position = slide_position - 5;
 	}
+	digitalWrite(LED_PIN, LOW);
 }
 
 void loop()
