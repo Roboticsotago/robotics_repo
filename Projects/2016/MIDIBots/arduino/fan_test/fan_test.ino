00585802b13da8dde74a3cbed7fa0903e64066fe
Chris Edwards <screwtop.github@ripples.dyndns.org>
2016-09-02 22:30:08 +1200

    Modified fan test program to allow interactive control of fan speed for testing

--- Projects/2016/MIDIBots/arduino/fan_test/fan_test.ino
+++ Projects/2016/MIDIBots/arduino/fan_test/fan_test.ino
@@ -1,15 +1,44 @@
-const int FAN_OUTPUT = 6;
+// FanBot speed test code
+// Control fan speed using serial data
+// "+" to increase speed
+// "-" to decrease speed
+
+
+const int FAN_OUTPUT = 6,
+	MAX_SPEED = 255,
+	MIN_SPEED = 0;
+
+int fan_speed = 0;
+int control_byte = 0;
 
 void setup()
 {
 	pinMode(FAN_OUTPUT, OUTPUT);
-	digitalWrite(FAN_OUTPUT, LOW);
+	analogWrite(FAN_OUTPUT, LOW);
+	Serial.begin(9600);
+}
+
+void speed_increase() {
+	fan_speed++;
+	if (fan_speed > MAX_SPEED) {fan_speed = MAX_SPEED;}
+	analogWrite(FAN_OUTPUT, fan_speed);
+}
+
+void speed_decrease() {
+	fan_speed--;
+	if (fan_speed < MIN_SPEED) {fan_speed = MIN_SPEED;}
+	analogWrite(FAN_OUTPUT, fan_speed);
 }
 
 void loop()
 {
-	analogWrite(FAN_OUTPUT, 64);
-	delay(5000);
-	analogWrite(FAN_OUTPUT, 0);
-	delay(2500);
+	if (Serial.available()) {
+		control_byte = Serial.read();
+		// TODO: error-checking?
+		switch (control_byte) {
+			case '+': speed_increase(); break;
+			case '-': speed_decrease(); break;
+		}
+		Serial.println(fan_speed);
+	}
 }
