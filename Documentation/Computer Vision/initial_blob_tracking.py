e32e7d9872b40ca538013bead700acff31f2a377
Tobias <tobz.monkeyman@gmail.com>
2016-11-01 18:08:01 +1300

    centroid co-ordinates and frame rate stuff

--- Documentation/Computer Vision/initial_blob_tracking.py
+++ Documentation/Computer Vision/initial_blob_tracking.py
@@ -1,30 +1,49 @@
 import SimpleCV
 import sys
-sys.path.append('.')
+sys.path.append('/home/pi/robotics_repo/Documentation/Computer Vision/infoscimechatronics')
 import cvutils
 import time
 
 camera = SimpleCV.Camera()
+os.system('/home/pi/robotics_repo/Projects/2017/SoccerBots/uvcdynctrl-settings.tcl')
 
-lab_grey_sample = (170.53383458646616, 179.7669172932331, 180.33082706766916)
-lab_goal_blue = ((103.0, 3.0), (177.0, 11.0), (163.0, 26.0))
+lab_grey_sample = cvutils.calibrate_white_balance()
+lab_goal_blue = cvutils.calibrate_colour_match(lab_grey_sample)
+
+times = []
+
+def average(numbers):
+        x = 0
+        for num in numbers:
+                x += num
+        x = x/len(numbers)
+        return x
 
 while True:
+        start_time = time.clock()
 	image = cvutils.wb(camera.getImage().scale(0.25), lab_grey_sample)
 	v,s,h = image.toHSV().splitChannels()
 	hue_match = h.colorDistance((lab_goal_blue[0][0],lab_goal_blue[0][0],lab_goal_blue[0][0])).binarize(lab_goal_blue[0][1]*3)
 	sat_match = s.colorDistance((lab_goal_blue[1][0],lab_goal_blue[1][0],lab_goal_blue[1][0])).binarize(lab_goal_blue[1][1]*3)
 	matched = ((hue_match / 16) * (sat_match / 16))
 	matched.show()
-	blobs = matched.findBlobs()
-	largest_blob = blobs[-1]
-	largest_blob.draw()
-	largest_blob_centroid = largest_blob.centroid()
-	largest_blob_centroid.draw()
-	image.drawCircle((largest_blob_centroid[0],largest_blob_centroid[1]),rad=5,color=SimpleCV.Color.VIOLET,thickness=1)
+	blobs = matched.findBlobs(100, 1)
+	if blobs is not None:
+		(x,y) = blobs[-1].centroid()
+		image.dl().line((x,0), (x,image.height), (255,0,0), antialias=False)
+		image.dl().line((0,y), (image.width,y), (0,255,0), antialias=False)
+		image.show()
+		blob_size = blobs[-1].area()
+                image_size = image.area()
+                print blob_size / image_size
+	else:
+		print "No blobs found!"
+	end_time = time.clock()
+	elapsed_time = end_time - start_time
+	times.append(elapsed_time)
+	frame_rate = 1 / average(times)
+	image.drawText(('Frame rate:' + str(frame_rate)), 0, 0)
 	image.show()
-	matched.show()
-	time.sleep(1)
-	
-	
 
+
+        
