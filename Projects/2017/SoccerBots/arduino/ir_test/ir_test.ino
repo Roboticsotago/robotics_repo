ba987b0d01144c8ef2f8197e1faa6716dc0f80a6
SoccerBot <henry.edenmann@gmail.com>
2017-06-15 20:48:40 +1200

    Added Chris's modifications to the ball IR direction/angle test program, currently with output formatted for Pure Data (FUDI), to be deleted so the kids can figure it out for themselves...

--- Projects/2017/SoccerBots/arduino/ir_test/ir_test.ino
+++ Projects/2017/SoccerBots/arduino/ir_test/ir_test.ino
@@ -1,4 +1,7 @@
+// TODO: main statement of the purpose of this code!
+
 #include <math.h>
+
 const int IR_1 = 2;
 const int IR_2 = 3;
 const int IR_3 = 4;
@@ -8,20 +11,19 @@ const int NUM_SENSORS = 8;
 const int analog_sensor_pins[] = {A0,A1,A2,A3,A4,A5,A6,A7};
 float ir_values[8];
 float IR_COORDINATES[NUM_SENSORS][2] = {{0.0,1.0},{0.71,0.71},{1.0,0.0},{0.71,-0.71},{0.0,-1.0},{-0.71, -0.71},{-1.0, 0.0},{-0.71, 0.71}};
-const int IR_THRESHOLD = 870; 
+const int IR_THRESHOLD = 920; // About 0.15 after converting to 0..1 float looked about right, which would be ~870 raw.
 
 int ir_val;
 const float TAU = 2 * PI;
 
 void setup() {
-
 	for(int n=0; n<NUM_SENSORS; n++) {
 		//pinMode(sensor_pins[n], INPUT);
                 pinMode(analog_sensor_pins[n], INPUT);
 
 	}
-	Serial.begin(9600);
-	Serial.println("Welcome Uno");
+	Serial.begin(115200);
+	//Serial.println("Welcome Uno");
 }
 
 void test_loop() {
@@ -29,7 +31,7 @@ void test_loop() {
     Serial.print(analogRead(analog_sensor_pins[n]) + " ");
   }
   Serial.println();
-  delay(100);
+  delay(50);
 }
 
 
@@ -37,7 +39,7 @@ void loop() {
 	readIRsensors();
 	//printIRsensors();
 	ball_angle();
-	delay(5000);
+	delay(50);
 }
 
 float readIRsensor(int sensor_num){
@@ -56,12 +58,14 @@ void readIRsensors(){
 }
 
 void printIRsensors(){
+	Serial.print("Raw sensor readings: ");
 	for (int i=0; i<NUM_SENSORS; i++) {
 		Serial.print(ir_values[i]);
 		Serial.print(" ");
 	} 
 	Serial.println();
 }
+
 void rad2degTest(){
 	for (int i=0; i < 8; i ++) {
 		Serial.println(i / 8.0 * TAU);
@@ -85,31 +89,51 @@ float normaliseDegrees(float d){
 }
 
 float ball_angle() {
-	Serial.println("ball angle called!");
+	//Serial.println("ball angle called!");
 	
 	float x_total = 0; float y_total = 0; int count = 0; 
 	for(int n=0; n<NUM_SENSORS; n++) {
 		x_total += IR_COORDINATES[n][0]*ir_values[n];
 		y_total += IR_COORDINATES[n][1]*ir_values[n];
+		/*
 		Serial.print(n);
 		Serial.print(" (");
 		Serial.print(IR_COORDINATES[n][0]*ir_values[n]);
 		Serial.print(",");
 		Serial.print(IR_COORDINATES[n][1]*ir_values[n]);
 		Serial.print(")");
+		*/
 		count += 1;
 	}
-
+	//Serial.println();
+	
 	float x_average = x_total/(float)count;
 	float y_average = y_total/(float)count;
 	
-	
+	/*
 	Serial.print("  X AVERAGE: ");
 	Serial.print(x_average);
 	Serial.print("  Y AVERAGE: ");
 	Serial.print(y_average);
 	Serial.println(" ");
+	*/
 	
+	// Calculate angle:
+	//Serial.print("; Angle = ");
+	Serial.print(degrees(atan2(x_average,y_average)));
+	Serial.print(" ");
+	
+	// Calculate approximate distance:
+	// First, the length of the vector (this is just the Pythagorean theorem):
+	float vector_length = sqrt(pow(x_average,2) + pow(y_average,2));
+	//Serial.print("; Vector Length = "); Serial.print(vector_length);
+	// Test distances: (vector_length, actual_distance):
+	// (0.2, 0.25 m), (0.1, 0.5 m), (0.09, 1 m), (0.08, 2 m)
+	float distance = exp(0.33/vector_length) * 0.03;
+	Serial.print(distance);
+	Serial.println(";");
+	//Serial.print("; Distance = "); Serial.print(distance); Serial.print(" m");
+	//Serial.println();
 }
 	//~ return normaliseDegrees(rad2deg(atan(y_average/x_average)));
 	//~ if(y_average > 0){
