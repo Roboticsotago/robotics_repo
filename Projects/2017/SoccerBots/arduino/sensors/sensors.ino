7eae8219e138b59ff858e0dc00d6adb88628be8a
SoccerBot <henry.edenmann@gmail.com>
2017-06-21 21:22:43 +1200

    Renamed IR_test to Sensors

---
+++ Projects/2017/SoccerBots/arduino/sensors/sensors.ino
@@ -0,0 +1,163 @@
+// TODO: main statement of the purpose of this code!
+
+#include <math.h>
+
+const int IR_1 = 2;
+const int IR_2 = 3;
+const int IR_3 = 4;
+const int IR_4 = 5;
+const int NUM_SENSORS = 8;
+//const int sensor_pins[] = {2,3,4,5,6,7,8,9};
+const int analog_sensor_pins[] = {A0,A1,A2,A3,A4,A5,A6,A7};
+float ir_values[8];
+float IR_COORDINATES[NUM_SENSORS][2] = {{0.0,1.0},{0.71,0.71},{1.0,0.0},{0.71,-0.71},{0.0,-1.0},{-0.71, -0.71},{-1.0, 0.0},{-0.71, 0.71}};
+const int IR_THRESHOLD = 980; // About 0.15 after converting to 0..1 float looked about right, which would be ~870 raw.  In practice, with no IR ball present, we never see a raw value less than 1000.
+
+
+#define DEBUGGING 0
+
+/*
+#define debug(message) \
+	do { if (DEBUGGING) DEBUG(message); } while (0)
+*/
+
+#ifdef DEBUGGING
+	#define DEBUG(x) Serial.println (x)
+	#define DEBUG_NOEOL(x) Serial.print (x)
+#else
+	#define DEBUG(x)
+	#define DEBUG_NOEOL(x)
+#endif
+
+
+int ir_val;
+const float TAU = 2 * PI;
+
+void setup() {
+	for(int n=0; n<NUM_SENSORS; n++) {
+		//pinMode(sensor_pins[n], INPUT);
+                pinMode(analog_sensor_pins[n], INPUT);
+
+	}
+	Serial.begin(115200);
+	//Serial.println("Welcome Uno");
+}
+
+void test_loop() {
+  for(int n=0; n<=NUM_SENSORS; n++) {
+    DEBUG_NOEOL(analogRead(analog_sensor_pins[n]) + " ");
+  }
+  DEBUG();
+  delay(50);
+}
+
+
+void loop() {
+	readIRsensors();
+	printIRsensors();
+	ball_angle();
+	delay(500);
+}
+
+float readIRsensor(int sensor_num){
+	int reading = analogRead(analog_sensor_pins[sensor_num]);
+	// TODO: debugging
+	DEBUG_NOEOL("sensor "); DEBUG_NOEOL(sensor_num); DEBUG_NOEOL(": "); DEBUG(reading);
+	if (reading>IR_THRESHOLD){
+		return 0.0;
+	}else{
+		return (1023-reading)/1023.0;
+	}
+}
+
+void readIRsensors(){
+	for (int i=0; i<NUM_SENSORS; i++) {
+		ir_values[i] = readIRsensor(i);
+	} 
+}
+
+void printIRsensors(){
+	DEBUG_NOEOL("Raw sensor readings: ");
+	for (int i=0; i<NUM_SENSORS; i++) {
+		DEBUG_NOEOL(ir_values[i]);
+		DEBUG_NOEOL(" ");
+	} 
+	DEBUG();
+}
+
+void rad2degTest(){
+	for (int i=0; i < 8; i ++) {
+		DEBUG(i / 8.0 * TAU);
+		DEBUG(normaliseDegrees(rad2deg(i / 8.0 * TAU)));
+	}
+}
+
+float rad2deg(float rad){
+	return (rad * -1.0 + TAU / 4.0) * (360.0 / TAU);
+}
+
+float normaliseDegrees(float d){
+	//return fmod(d + 180, 360); 
+	if (d < -180){
+		return d + 360;
+	}else if(d>180){
+		return d - 360;
+	}else{
+		return d;
+	}
+}
+float vector2distance(float vector_magnitude){
+       return exp(0.33/vector_magnitude) * 0.03;
+}
+float ball_angle() {
+	//DEBUG("ball angle called!");
+	
+	// Calculate the centroid of the ball detection vectors...
+	
+	float x_total = 0; float y_total = 0; int count = 0; 
+	for(int n=0; n<NUM_SENSORS; n++) {
+		x_total += IR_COORDINATES[n][0]*ir_values[n];
+		y_total += IR_COORDINATES[n][1]*ir_values[n];
+		/*
+		DEBUG_NOEOL(n);
+		DEBUG_NOEOL(": (");
+		DEBUG_NOEOL(IR_COORDINATES[n][0]*ir_values[n]);
+		DEBUG_NOEOL(",");
+		DEBUG_NOEOL(IR_COORDINATES[n][1]*ir_values[n]);
+		DEBUG_NOEOL("); ");
+		*/
+		count += 1;
+	}
+	//DEBUG();
+	
+	float x_average = x_total/(float)count;
+	float y_average = y_total/(float)count;
+	
+	/*
+	DEBUG_NOEOL("  X AVERAGE: ");
+	DEBUG_NOEOL(x_average);
+	DEBUG_NOEOL("  Y AVERAGE: ");
+	DEBUG_NOEOL(y_average);
+	DEBUG(" ");
+	*/
+	
+	// Calculate angle:
+	// This will use the atan2() function to determine the angle from the average x and y co-ordinates
+	// You can use the degrees() function to convert for output/debugging.
+	float angle = atan2(x_average, y_average);
+        DEBUG_NOEOL("Angle to ball: ");
+        DEBUG_NOEOL(degrees(angle));
+	
+	// Calculate approximate distance:
+	// First, determine the length of the vector (use the Pythagorean theorem):
+	float vector_magnitude = sqrt(pow(x_average, 2)+pow(y_average,2));	// TODO: your code here
+        
+	// We need to map the raw vector magnitudes to real-world distances. This is probably not linear! Will require some calibration testing...
+	float distance = vector2distance(vector_magnitude); // TODO: your code here
+	DEBUG_NOEOL(" Vector magnitude: ");
+	DEBUG_NOEOL(vector_magnitude);
+	DEBUG_NOEOL(" Distance: ");
+	DEBUG_NOEOL(distance);
+	DEBUG("");
+	// TODO: maybe also check for infinity, and map that to a usable value (e.g. 0).
+}
