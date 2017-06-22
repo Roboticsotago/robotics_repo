be91267ee48cdf540ad206ad0bee109de1462f25
SoccerBot <henry.edenmann@gmail.com>
2017-06-23 00:10:10 +1200

    Added puredata paths to runeverything shell script

--- Projects/2017/SoccerBots/runeverything.sh
+++ Projects/2017/SoccerBots/runeverything.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pd -noaudio -nomidi patch-filename.pd &
+pd -noaudio -nomidi motor-control.pd attacker_main.pd sensor_input.pd &
 python2.7 initial_blob_tracking.py | pdsend 7001 &
 cu -l ttyUSB0 -s 115200 | pdsend 7000
 #TODO: change ttyUSB0 to device ID
