d708bcc7bff9c097a7e048c195f177384ffcedfe
Chris Edwards <screwtop.github@ripples.dyndns.org>
2017-06-29 16:46:06 +1200

    Modified runeverything.sh for changed location/name of blob_tracking.py

--- Projects/2017/SoccerBots/runeverything.sh
+++ Projects/2017/SoccerBots/runeverything.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pd -noaudio -nomidi motor-control.pd attacker_main.pd sensor_input.pd &
 sleep 5
-python2.7 initial_blob_tracking.py | pdsend 7001 &
+python2.7 blob_tracking.py | pdsend 7001 & 
 cu -l /dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0 -s 115200 | pdsend 7000
 #TODO: change ttyUSB0 to device ID
