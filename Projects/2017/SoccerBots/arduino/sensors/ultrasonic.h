b65ba24f994c1879837a7ad604d29f21e6b08bff
SoccerBot <henry.edenmann@gmail.com>
2017-06-30 19:19:54 +1200

    Added link to DFRobot documentation, explanatory notes

--- Projects/2017/SoccerBots/arduino/sensors/ultrasonic.h
+++ Projects/2017/SoccerBots/arduino/sensors/ultrasonic.h
@@ -1,8 +1,11 @@
+// NOTE: DFRobot sensor board must be jumpered correctly for TTL operation and have been programmed (EEPROM via serial) to passive PWM mode!
+// See https://www.dfrobot.com/wiki/index.php/URM37_V3.2_Ultrasonic_Sensor_(SKU:SEN0001)
+
 const int trig = 5;
 const int pwm = 8;
-const int unit_division_factor = 40;
+const int unit_division_factor = 40; // Supposedly 50 us/cm but Tobias found that 40 gave better accuracy.
 int us_distance;
-const int RANGE_SENSOR_TIMEOUT = 15000; // us
+const int RANGE_SENSOR_TIMEOUT = 15000; // microseconds for pulseIn() timeout (to avoid excessive blocking delay)
 
 
 void ultrasonic_setup() {
