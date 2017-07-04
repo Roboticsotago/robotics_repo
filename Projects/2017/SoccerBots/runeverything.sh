a1d09dbbac60dd1f5ced5423c8048a096978a27e
SoccerBot <henry.edenmann@gmail.com>
2017-07-04 17:35:09 +1200

    main programme for goalie and running different pd script depending on the robot

--- Projects/2017/SoccerBots/runeverything.sh
+++ Projects/2017/SoccerBots/runeverything.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
-pd -noaudio -nomidi motor-control.pd attacker_main.pd sensor_input.pd &
+#!/bin/bash
+if [[ `hostname` == "Boris" ]]; then
+	pd_patch=goalie_main.pd
+	echo Goalie
+else
+	pd_patch=attacker_main.pd
+	echo Attacker
+fi
+pd -noaudio -nomidi motor-control.pd $pd_patch sensor_input.pd &
 sleep 5
 v4l2-ctl -c  power_line_frequency=1 -c auto_exposure=1 -c exposure_time_absolute=150 -c white_balance_auto_preset=3 -c  iso_sensitivity=4 -c iso_sensitivity_auto=0
 python2.7 blob_tracking.py | pdsend 7001 & 
