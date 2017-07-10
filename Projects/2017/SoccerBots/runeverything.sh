bfb21651ae5dc5722efb2c7871f85debc537b973
SoccerBot <henry.edenmann@gmail.com>
2017-07-10 15:48:28 +1200

    working on commands to open each different programme in its own terminal

--- Projects/2017/SoccerBots/runeverything.sh
+++ Projects/2017/SoccerBots/runeverything.sh
@@ -6,8 +6,8 @@ else
 	pd_patch=attacker_main.pd
 	echo Attacker
 fi
-pd -noaudio -nomidi motor-control.pd $pd_patch sensor_input.pd &
+lxterminal -t "pd" -e "pd -noaudio -nomidi motor-control.pd $pd_patch sensor_input.pd"
 sleep 5
 v4l2-ctl -c  power_line_frequency=1 -c auto_exposure=1 -c exposure_time_absolute=150 -c white_balance_auto_preset=3 -c  iso_sensitivity=4 -c iso_sensitivity_auto=0
-python2.7 blob_tracking.py | pdsend 7001 & 
-./read_sensors.tcl | pdsend 7000 &
+lxterminal -t "blob tracking" -e "python2.7 blob_tracking.py | pdsend 7001"
+urxvt -title "read_sensors.tcl" -e sh "./read_sensors.tcl | pdsend 7000"
