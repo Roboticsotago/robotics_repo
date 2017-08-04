66a8c4bdc4a41d0bdd579af4717bf50fb83308e3
SoccerBot <henry.edenmann@gmail.com>
2017-08-04 19:07:13 +1200

    pd now starts with DSP on

--- Projects/2017/SoccerBots/runeverything.sh
+++ Projects/2017/SoccerBots/runeverything.sh
@@ -6,7 +6,7 @@ else
 	pd_patch=attacker_main.pd
 	echo Attacker
 fi
-urxvt -name Pd -title Pd -e sh -c "pd -noaudio -nomidi motor-control.pd $pd_patch sensor_input.pd" &
+urxvt -name Pd -title Pd -e sh -c "pd -noaudio -nomidi -send \"; pd dsp 1\" motor-control.pd $pd_patch sensor_input.pd" &
 # TODO: replace sleep with until loop:
 sleep 5
 v4l2-ctl -c  power_line_frequency=1 -c auto_exposure=1 -c exposure_time_absolute=150 -c white_balance_auto_preset=3 -c  iso_sensitivity=4 -c iso_sensitivity_auto=0
