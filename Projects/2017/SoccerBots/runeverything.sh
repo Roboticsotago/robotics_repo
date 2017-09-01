104eadd142bd29ca1ef3d7860001f0368bdbc525
SoccerBot <henry.edenmann@gmail.com>
2017-09-01 21:53:12 +1200

    changed display enviroment variable to show SimpleCV preview on local window to decrease CPU usage

--- Projects/2017/SoccerBots/runeverything.sh
+++ Projects/2017/SoccerBots/runeverything.sh
@@ -9,6 +9,7 @@ fi
 urxvt -name Pd -title Pd -e sh -c "pd -noaudio -nomidi -send \"; pd dsp 1\" motor-control.pd $pd_patch sensor_input.pd" &
 # TODO: replace sleep with until loop:
 sleep 5
+urxvt -name read_sensors -title read_sensors -e sh -c "./read_sensors.tcl | pdsend 7000" &
+export DISPLAY=:0.0
 v4l2-ctl -c  power_line_frequency=1 -c auto_exposure=1 -c exposure_time_absolute=150 -c white_balance_auto_preset=3 -c  iso_sensitivity=4 -c iso_sensitivity_auto=0
 urxvt -name blob_tracking -title blob_tracking -e sh -c "python2.7 blob_tracking.py | pdsend 7001" &
-urxvt -name read_sensors -title read_sensors -e sh -c "./read_sensors.tcl | pdsend 7000" &
