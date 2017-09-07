1289a2011a6d3f8caef890c93928d9b1c89f08d5
SoccerBot <henry.edenmann@gmail.com>
2017-09-07 19:28:45 +1200

    Tentatively added Pd 0.48 command line, but reverting to the Raspbian system one since we need the hcs and cyclone packages. But it now runs only audio output and at only 8 kHz sampling rate, which might help lower CPU use.

--- Projects/2017/SoccerBots/runeverything.sh
+++ Projects/2017/SoccerBots/runeverything.sh
@@ -6,7 +6,10 @@ else
 	pd_patch=attacker_main.pd
 	echo Attacker
 fi
-urxvt -name Pd -title Pd -e sh -c "pd -noaudio -nomidi -send \"; pd dsp 1\" motor-control.pd $pd_patch sensor_input.pd" &
+# For new locally-installed Pd 0.48 (will need hcs and cyclone libraries too):
+#urxvt -name Pd -title Pd -e sh -c "/usr/local/bin/pd -nomidi -alsa -noadc -r 8000 -send \"; pd dsp 1\" motor-control.pd $pd_patch sensor_input.pd" &
+# For system Pd package:
+urxvt -name Pd -title Pd -e sh -c "/usr/bin/pd -nomidi -alsa -noadc -r 8000 -send \"; pd dsp 1\" motor-control.pd $pd_patch sensor_input.pd" &
 # TODO: replace sleep with until loop:
 sleep 5
 urxvt -name read_sensors -title read_sensors -e sh -c "./read_sensors.tcl | pdsend 7000" &
