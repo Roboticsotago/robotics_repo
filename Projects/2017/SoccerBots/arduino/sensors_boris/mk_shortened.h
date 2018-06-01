da46c79dbf359bd6c5f42d07532e57156787a338
SoccerBot <henry.edenmann@gmail.com>
2018-06-01 21:29:45 +1200

    split up sensor program files into Shutter and Boris - Boris isn't working and Shutter is untested, yet we expect it to work

---
+++ Projects/2017/SoccerBots/arduino/sensors_boris/mk_shortened.h
@@ -0,0 +1,6 @@
+//shortened version of In The Hall of The Mountain King for Shutter on startup
+
+const int mk_shortened_tempo = 150;
+const int mk_shortened_array_length = 13;
+float mk_shortened[mk_shortened_array_length][2] = {{62,0.5},{64,0.5},{65,0.5},{67,0.5},{69,0.5},{65,0.5},{69,0.5},{74,0.5},
+	{73,0.5},{69,0.5},{73,0.5},{76,0.5},{74,2}}; 
