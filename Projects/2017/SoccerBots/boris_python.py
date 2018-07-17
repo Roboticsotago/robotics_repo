3a373c13a67a66824614a22f231aef8196014d36
SoccerBot <henry.edenmann@gmail.com>
2018-07-17 18:06:01 +1200

    minor changes

--- Projects/2017/SoccerBots/boris_python.py
+++ Projects/2017/SoccerBots/boris_python.py
@@ -2,9 +2,9 @@ import serial
 import time
 serinput = serial.Serial('/dev/serial/by-id/usb-Arduino__www.arduino.cc__0042_852313632363516031B2-if00', 115200)
 seroutput = serial.Serial('/dev/serial/by-id/usb-DSpace__www.dspace.org.nz__DSpace_Robot_2.0_55234313335351A0D161-if00', 19200)
-left_motor_mask = 0b10000000 #first bit 1 means motor message
-right_motor_mask = 0b11000000 #second bit 1 means right motor
-kicker_mask = 0b00000000 #first bit 0 means kicker message
+left_motor_string = 0b10000000 #first bit 1 means motor message
+right_motor_string = 0b11000000 #second bit 1 means right motor
+kicker_mask = 0b00000000 #first bit -  0 means kicker message
 speed_threshold = 0.4 #deadzone threshold
 
 
@@ -27,24 +27,22 @@ def readsensors():
         time.sleep(1)
 
 
-def motoroutput(binary_input1, binary_input2):
-    if binary_input1 is not None:
-        x = left_motor(binary_input1).strip("0b")
-        print(x)
-        seroutput.write(str(x).encode())
-    if binary_input2 is not None:
-        y = right_motor(binary_input2).strip("0b")
-        print(y)
-        seroutput.write(str(y).encode())
-    else:
-        pass
-
+def motoroutput(left, right):
+    x = left_motor(left).strip("0b")
+    print(x)
+    seroutput.write(str(x).encode())
+    y = right_motor(right).strip("0b")
+    print(y)
+    seroutput.write(str(y).encode())
+    
 
 def deadzone(speed, threshold):
     if(speed > 0):
         out = speed + threshold
     elif(speed < 0):
         out = speed - threshold
+    elif(speed==0):
+		out = 0
     if(out<-1):
         return -1
     elif(out > 1):
@@ -55,20 +53,20 @@ def deadzone(speed, threshold):
 def left_motor(speed): # input -1 to +1
     speed = deadzone(speed, speed_threshold)
     if speed >= 0:
-        dir_bit_mask = 0b00100000
+        dir_bit_string = 0b00100000
     else:
-        dir_bit_mask = 0b00000000
+        dir_bit_string = 0b00000000
     speedbits = (int(abs(speed)*255))>>3
-    return bin(dir_bit_mask | speedbits | left_motor_mask)
+    return bin(dir_bit_string | speedbits | left_motor_string)
 
 def right_motor(speed):
     speed = deadzone(speed, speed_threshold)
     if speed >= 0:
-        dir_bit_mask = 0b00100000
+        dir_bit_string = 0b00100000
     else:
-        dir_bit_mask = 0b00000000
+        dir_bit_string = 0b00000000
     speedbits = (int(abs(speed)*255))>>3
-    return bin(dir_bit_mask | speedbits | right_motor_mask)
+    return bin(dir_bit_string | speedbits | right_motor_string)
 
 def kicker(kick):
     return bin(kicker_mask | kick)
@@ -76,5 +74,8 @@ def kicker(kick):
 
 while True:
     motoroutput(-1,-1)
+    time.sleep(1)
+    #motoroutput(1,1)
+    #time.sleep(1)
     #readsensors()
     #print(ball_angle)
