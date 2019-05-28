b1fa6754ea03a9d967b96011da73f18fe6b5c48c
Chris Edwards <chris.edwards@otago.ac.nz>
2019-05-28 17:59:52 +1200

    stuff

--- Projects/2018/Rubik's Cube/stepper_test/stepper_test.ino
+++ Projects/2018/Rubik's Cube/stepper_test/stepper_test.ino
@@ -3,7 +3,7 @@ const int degrees_per_step = 360 / steps_per_rev;
 const int motor_pin = 10;
 
 void spin(float rpm, float num_rotations) {
-  min_delay = 300/rpm - 1
+  float min_delay = 300/rpm - 1;
   for(int i = 0; i <= num_rotations*200; i++) {
     digitalWrite(motor_pin, HIGH);
     delay(min_delay);
@@ -12,26 +12,31 @@ void spin(float rpm, float num_rotations) {
   }
 }
 
-void accel(float rpm) {
+void accel(float rpm, float ramp_up) {
   for(float current_rpm = 0; current_rpm <= rpm; current_rpm += rpm / 10) {
-    spin(current_rpm, 0.1);
+    spin(current_rpm, ramp_up/10);
   } 
 }
 
-void decel(float initial_rpm) {
+void decel(float initial_rpm, float ramp_down) {
   for(float current_rpm = initial_rpm; current_rpm >= 0; current_rpm -= initial_rpm / 10) {
-    spin(current_rpm, 0.1);
+    spin(current_rpm, ramp_down);
   }
-  digitalWrite(motor_pin, HIGH)
+  digitalWrite(motor_pin, HIGH);
+}
+
+void ninety() { //total: 0.25 rotations
+   accel(10,);
+   spin(10,);
+   decel(10,);
 }
 
 void setup() {
   pinMode(motor_pin, OUTPUT);
+  accel(10);
 }
 
 void loop() {
-  accel(60);
-  spin(60, 10);
-  decel(60);
-  delay(5000);
+  spin(10, 100); 
+  //digitalWrite(motor_pin, HIGH); 
 }
