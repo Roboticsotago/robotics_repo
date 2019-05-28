3a57aa27e1e8c1840a857e9d2565bdbe9ec73745
MonkeymanZ13 <tobz.monkeyman@gmail.com>
2019-05-28 13:41:14 +1200

    worked on acceleration and deceleration functions

--- Projects/2018/Rubik's Cube/stepper_test/stepper_test.ino
+++ Projects/2018/Rubik's Cube/stepper_test/stepper_test.ino
@@ -1,27 +1,37 @@
-void setup() {
-  pinMode(10, OUTPUT);
-}
-
 const int steps_per_rev = 200;
 const int degrees_per_step = 360 / steps_per_rev;
+const int motor_pin = 10;
 
-
-void spin(float rpm) {
-  for(int x = 1000; x >= ; x -= 50) {
-    digitalWrite(10, HIGH);
-    delay(x);
-    digitalWrite(10, LOW);
+void spin(float rpm, float num_rotations) {
+  min_delay = 300/rpm - 1
+  for(int i = 0; i <= num_rotations*200; i++) {
+    digitalWrite(motor_pin, HIGH);
+    delay(min_delay);
+    digitalWrite(motor_pin, LOW);
     delay(1);
   }
-  float rps = rpm / 60;
-  float steps_per_sec = rps / degrees_per_step;
-  digitalWrite(10, HIGH);
-  delay(1/steps_per_sec * 1000 - 1);
-  digitalWrite(10, LOW);
-  delay(1);
 }
 
+void accel(float rpm) {
+  for(float current_rpm = 0; current_rpm <= rpm; current_rpm += rpm / 10) {
+    spin(current_rpm, 0.1);
+  } 
+}
+
+void decel(float initial_rpm) {
+  for(float current_rpm = initial_rpm; current_rpm >= 0; current_rpm -= initial_rpm / 10) {
+    spin(current_rpm, 0.1);
+  }
+  digitalWrite(motor_pin, HIGH)
+}
+
+void setup() {
+  pinMode(motor_pin, OUTPUT);
+}
 
 void loop() {
-  spin(60);
+  accel(60);
+  spin(60, 10);
+  decel(60);
+  delay(5000);
 }
